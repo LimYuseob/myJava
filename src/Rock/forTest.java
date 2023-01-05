e966a928021d64347b5d877a7eff646ef5720c10
slcp-014g <slcp-014g@slcp-014>
2023-01-05 17:48:53 +0900

    자바팀프로젝트 개인작업

---
+++ src/Rock/forTest.java
@@ -0,0 +1,14 @@
+package Rock;
+
+public class forTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이제 된거냐?!!!!!");
+		System.out.println("으아 힘들다 20:24");
+		System.out.println("20230105  1709");
+		System.out.println("20230105  1709");
+		System.out.println("20230105  1714");
+		System.out.println("20230105  1716");
+	}
+}
