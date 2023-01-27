cb3ff7e8f20f75aab6a121023863e3993d0a8524
byul <byul@byulhouse>
2023-01-27 15:06:10 +0900

    ddd

--- src/projetctgu/Menu4.java
+++ src/projetctgu/Menu4.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
+import java.util.List; 
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
